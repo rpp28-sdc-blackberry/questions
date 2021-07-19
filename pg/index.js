80f5c7ad3841034bab03f0089b28d42c324d750e
Farhan Ali <fahanali3193@gmail.com>
2021-07-19 15:09:54 -0700

    Added GET/questions route.

--- pg/index.js
+++ pg/index.js
@@ -4,14 +4,19 @@ const client = new Client({
 });
 
 client.connect()
-  .then(() => console.log('CONNECTED'))
+  .then(() => console.log('CONNECTED TO PG'))
   .catch((err) => console.log('ERROR CONNECTING TO PG', err))
 
-let text = `SELECT * FROM questions offset 3518960`;
-client.query(text)
-  .then((res) => {
-    console.log('RESPONSE', res.rows);
-  })
-  .catch((err) => {
-    console.log('ERROR GETTING RESPONSE', err);
-  })
+let getQuestions = (product_id, offset, limit) => {
+  let questionsQuery = `SELECT * FROM questions WHERE product_id = ${product_id} ORDER BY question_id ASC OFFSET ${offset} LIMIT ${limit}`;
+  return client.query(questionsQuery)
+    .then((res) => {
+      // console.log('RESPONSE', res.rows);
+      return res.rows;
+    })
+    .catch((err) => {
+      console.log('ERROR GETTING RESPONSE', err);
+    })
+}
+
+  module.exports.getQuestions = getQuestions;
--- server/index.js
+++ server/index.js
@@ -5,10 +5,31 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const AWS = require("aws-sdk");
+const db = require('../pg/index.js');
 
 const app = express();
 app.use(bodyParser.json({limit: '100mb'}));
 app.use(cors());
+
+app.get(`/qa/questions`, (req, res) => {
+  let { product_id, page, count } = req.query;
+  if (page === undefined) {
+    page = 0;
+  }
+  if (count === undefined) {
+    count = 5;
+  }
+
+  let offset = (page - 1) * count;
+
+  return db.getQuestions(product_id, offset, count)
+    .then((q) => {
+      console.log('Q', q);
+    })
+    .catch((err) => {
+      console.log('ERROR IN SERVER GETTING ALL Q', err)
+    });
+});
 
 const port = 3000;
 app.listen(port, () => {
