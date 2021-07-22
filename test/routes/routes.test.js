a5a3f79217ee0ae73902a0520493b69d5dc95046
Farhan Ali <fahanali3193@gmail.com>
2021-07-22 02:02:49 -0700

    Removed dummy file. Added beforeEAch and afterEach hooks to truncate and inset tdata in tbales

--- test/routes/routes.test.js
+++ test/routes/routes.test.js
@@ -11,9 +11,24 @@ const db = require('../../pg/index.js');
 //     .catch((err) => console.log('TEST PG NOT CONNECTED', err))
 // })
 
+beforeEach(async () => {
+  await db.client.query(`ALTER SEQUENCE questions_question_id_seq RESTART`);
+  await db.client.query(`ALTER SEQUENCE answers_answer_id_seq RESTART`);
+  await db.client.query(`ALTER SEQUENCE photos_id_seq RESTART`);
+
+  await db.client.query(`INSERT INTO questions (product_id, question_body, asker_name, asker_email) VALUES(1, 'test body1', 'test1', 'test1@g.com'),(2, 'test body2', 'test2', 'test2@g.com')`);
+  await db.client.query(`INSERT INTO answers (question_id, body, answerer_name, answerer_email) VALUES(1, 'answer body1', 'answer1', 'answer1@g.com'),(2, 'answer body2', 'answer2', 'answer2@g.com')`);
+  await db.client.query(`INSERT INTO photos (answer_id, url) VALUES(1, 'photo1'), (1, 'photo2'), (1, 'photo3'), (2, 'photo4')`);
+})
+
+afterEach(async () => {
+  await db.client.query(`TRUNCATE questions, answers, photos`);
+})
+
 afterAll(async () => {
   await db.client.end();
 });
+
 
 describe('GET/dummy', () => {
   it('responds with 200 status code', (done) => {
@@ -23,10 +38,11 @@ describe('GET/dummy', () => {
 });
 
 describe('GET/qa/questions', () => {
-  it('responds with 200 status code', async () => {
+  it('should respond with 200 status code and all questions', async () => {
     let response = await request.get('/qa/questions?product_id=1&count=1')
-    console.log('RESPONSE', response.body)
+    // console.log('RESPONSE', response.body.results[0])
     expect(response.statusCode).toBe(200);
-    expect(response.body.product_id).toEqual('1')
+    expect(response.body.product_id).toEqual('1');
+    expect(response.body.results.length).toEqual(1);
   }, 1000 * 30);
 });
