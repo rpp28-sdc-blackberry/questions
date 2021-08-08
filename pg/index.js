d9342a43c42b2ae22bdb95223bfc6959153865bf
Farhan Ali <fahanali3193@gmail.com>
2021-08-07 23:37:20 -0700

    Removed test functions in model

--- pg/index.js
+++ pg/index.js
@@ -22,34 +22,6 @@ client.connect()
   .catch((err) => console.log(`ERROR CONNECTING TO PG: DATABASE ${database}`, err))
 
 let getQuestions = (product_id, offset, limit) => {
-  // console.log('OFSET', offset, 'limit', limit)
-  let questionsQuery = `SELECT * FROM questions WHERE product_id = $1 ORDER BY question_id ASC OFFSET $2 LIMIT $3`;
-  let values = [product_id, offset, limit];
-
-  return client.query(questionsQuery, values)
-    .then(async (res) => {
-      try {
-        // console.log('DB ALL QUESTIONS', res.rows);
-        let questions = res.rows;
-
-        for (let question of questions) {
-          let answersQuery = `SELECT * FROM answers WHERE question_id = $1 ORDER BY answer_id`
-          let answers = await getAnswers(question.question_id, null, null, answersQuery);
-          question.answers = answers;
-        }
-        // console.log('DB QUESTIONS AFTER ADDING ALL ANSWERS', questions);
-        return questions;
-      } catch(err) {
-        console.log('ERROR AWAITING FOR ANSWERS', err)
-      }
-    })
-    .catch((err) => {
-      console.log('DB ERROR GETTING ALL QUESTIONS', err);
-      throw err;
-    })
-};
-
-let getQuestionsTest = (product_id, offset, limit) => {
   let questionsQuery = `SELECT questions.question_id, questions.question_body, questions.question_date, questions.asker_name, questions.reported, questions.question_helpfulness, answers.answer_id AS id, answers.body, answers.date, answers.answerer_name, answers.reported AS answer_reported, answers.helpfulness, photos.url AS photo_url FROM questions LEFT OUTER JOIN answers ON (questions.question_id = answers.question_id) LEFT OUTER JOIN photos ON (answers.answer_id = photos.answer_id) WHERE questions.product_id = $1 ORDER BY questions.question_id`;
   let values = [product_id];
 
@@ -141,41 +113,8 @@ let getQuestionsTest = (product_id, offset, limit) => {
       throw err;
     })
 }
-// getQuestionsTest(990000, 0, 5)
 
-let getAnswers = (question_id, offset, limit, answersQuery) => {
-  let values;
-  if (answersQuery === undefined) {
-    answersQuery = `SELECT * FROM answers WHERE question_id = $1 ORDER BY answer_id ASC OFFSET $2 LIMIT $3`;
-    values = [question_id, offset, limit];
-  } else {
-    values = [question_id];
-  }
-
-  return client.query(answersQuery, values)
-    .then(async (res) => {
-      try {
-        // console.log('DB ALL ANSWERS', res.rows);
-        let answers = res.rows;
-
-        for (let answer of answers) {
-          let photos = await getPhotos(answer.answer_id);
-          answer.photos = photos;
-        }
-
-        // console.log('DB ANSWERS AFTER ADDING ALL PHOTOS', answers);
-        return answers;
-      } catch(err) {
-        console.log('ERROR AWAITING FOR PHOTOS', err)
-      }
-    })
-    .catch((err) => {
-      console.log('DB ERROR GETTING ALL ANSWERS', err);
-      throw err;
-    })
-}
-
-let getAnswersTest = (question_id, offset, limit) => {
+let getAnswers = (question_id, offset, limit) => {
   let answersQuery = `SELECT answers.answer_id, answers.body, answers.date, answers.answerer_name, answers.reported, answers.helpfulness, photos.id AS photo_id, photos.url AS photo_url FROM answers LEFT OUTER JOIN photos ON (answers.answer_id = photos.answer_id) WHERE answers.question_id=$1 ORDER BY answers.answer_id`;
   let values = [question_id];
 
@@ -236,21 +175,6 @@ let getAnswersTest = (question_id, offset, limit) => {
       throw err;
     })
 }
-
-// getAnswersTest(1);
-
-// let getPhotos = (answer_id) => {
-//   let photosQuery = `SELECT id, url FROM photos WHERE answer_id = ${answer_id} ORDER BY id ASC`;
-//   return client.query(photosQuery)
-//     .then((res) => {
-//       // console.log('DB ALL PHOTOS', res.rows);
-//       return res.rows;
-//     })
-//     .catch((err) => {
-//       console.log('DB ERROR GETTING ALL PHOTOS', err);
-//       throw err;
-//     })
-// };
 
 let addQuestion = ({ product_id, body, name, email }) => {
   if (typeof product_id !== 'number' || typeof body !== 'string' || typeof name !== 'string' || typeof email !== 'string') {
@@ -371,15 +295,3 @@ module.exports.updateQuestionHelpfulness = updateQuestionHelpfulness;
 module.exports.reportQuestion = reportQuestion;
 module.exports.updateAnswerHelpfulness = updateAnswerHelpfulness;
 module.exports.reportAnswer = reportAnswer;
-module.exports.getQuestionsTest = getQuestionsTest;
-module.exports.getAnswersTest = getAnswersTest;
-
-//JOIN answers and photos table using a left join.
-// client.query(`SELECT answers.answer_id, answers.question_id, answers.body, answers.answerer_name, answers.answerer_email, answers.helpfulness, photos.id, photos.answer_id, photos.url FROM answers LEFT JOIN photos ON answers.answer_id = photos.answer_id OFFSET 0 LIMIT 5`)
-//   .then((res) => {
-//     console.log('DB ALL PHOTOS', res.rows);
-//     // return res.rows;
-//   })
-//   .catch((err) => {
-//     console.log('DB ERROR GETTING ALL PHOTOS', err);
-//   })
